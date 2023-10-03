84b4347954b5f0d45205a9d5fea024195c0afbe5
Michel Kramer <kramermichelsc@gmail.com>
2023-10-03 20:25:22 -0300

    inserimos a subtração

--- m_dia.py
+++ m_dia.py
@@ -3,5 +3,7 @@ numero = int(input('Informe um valor '))
 numero1 = int(input('Informe um segundo valor'))
 soma = numero + numero1
 multiplicacao = numero * numero1
+sub = numero - numero1
 print('Resultado da soma dos valores: ', soma)
 print('Resultado da multiplicação dos valores: ', multiplicacao)
+print('Resultado da multiplicação dos valores: ', sub)
