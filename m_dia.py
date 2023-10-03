14c7e08c1b5a8e4ba9af19cd6912b9f22e75f247
kramermichelsc <145510714+kramermichelsc@users.noreply.github.com>
2023-10-03 19:31:38 -0300

    Update m_dia.py

--- m_dia.py
+++ m_dia.py
@@ -1,3 +1,5 @@
 print("Boa Noite! Hoje é dia 03/10")
-numero = input('Informe um valor')
-print('O valor informado foi: ', numero)
+numero = input('Informe um valor ')
+numero1 = input('Informe um segundo valor')
+soma = numero + numero1
+print('Resultado da soma dos valores: ', soma)
